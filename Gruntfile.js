1a41af63eede5b1074407b450b1c3feda05769af
Abdul Rehman Talat <rehman.talat@gmail.com>
2016-08-28 14:15:50 +0500

    Added SCSS Linting

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,20 @@ module.exports = function(grunt) {
     //     dest: 'build/<%= pkg.name %>.min.js'
     //   }
     // },
+    // 
+    scsslint: {
+      options: {
+        bundleExec: true,
+        config: 'scss/.scss-lint.yml',
+        reporterOutput: null
+      },
+      core: {
+        src: ['scss/*.scss', '!scss/_normalize.scss']
+      },
+      // docs: {
+      //   src: ['docs/assets/scss/*.scss', '!docs/assets/scss/docs.scss']
+      // }
+    },
     
     sass: {                              // Task
       dist: {                            // Target
@@ -28,16 +42,37 @@ module.exports = function(grunt) {
           'dist/css/bootstrap.css': 'scss/bootstrap.scss'
         }
       }
+    },
+
+
+    jekyll: {                             // Task
+      options: {                          // Universal options
+        bundleExec: true
+      },
+      serve: {                            // Another target
+        options: {
+          serve: true,
+          // dest: '.jekyll',
+          drafts: true,
+          future: true
+        }
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-scss-lint');
 
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
   grunt.registerTask('default', ['sass']);
+
+  // JS distribution task.
+  // grunt.registerTask('dist-js', ['babel:dev', 'concat', 'babel:dist', 'stamp', 'uglify:core']);
+
+  grunt.registerTask('test-scss', ['scsslint:core']);
 
 };
